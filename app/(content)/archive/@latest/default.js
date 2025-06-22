e9ce0a37e822e6c3259ec24eeaade0fd3e42af4f
Saksham Jain <wgd1337.sakshamjain@gmail.com>
2025-06-23 00:10:27 +0530

    Initial commit

---
+++ app/(content)/archive/@latest/default.js
@@ -0,0 +1,14 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news"
+
+export default async function LatestNewsPage() {
+    const latestNews = await getLatestNews();
+
+    return(
+        <>
+        <h2>Latest News Page</h2>
+        <NewsList news={latestNews} /> 
+        </>
+    )
+    
+}
